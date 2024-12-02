ce6de7c298a613351f961cf40de0a9e335940592
saeidex <ebrahim.khalil.nayeem@gmail.com>
2024-12-02 12:21:38 +0600

    add: notiion loader

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection } from "astro:content";
+import { notionLoader } from "notion-astro-loader";
+
+const database = defineCollection({
+  loader: notionLoader({
+    auth: import.meta.env.NOTION_TOKEN,
+    database_id: import.meta.env.NOTION_DATABASE_ID,
+    filter: {
+      property: "Status",
+      status: { equals: "Live" },
+    },
+  }),
+});
+
+export const collections = { database };
