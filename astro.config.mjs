0cd79f23becc9629126da148e1892873bbb153b2
saeidex <ebrahim.khalil.nayeem@gmail.com>
2024-12-03 13:47:46 +0600

    add: seo stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,17 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://notion-astro-loader-example.pages.dev/",
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     react(),
+    sitemap(),
   ],
   experimental: {
     contentLayer: true,
